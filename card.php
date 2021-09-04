1398a93c47035c341991aff8f733241fb5e8b124
Lenilson Sillva <lenilson_sillva@yahoo.com>
2021-09-04 20:36:33 -0300

    change ok

--- card.php
+++ card.php
@@ -10,7 +10,7 @@ include('connections/confirm.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/global/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="styles/style.css">
     <title>Ebana Burger</title>
 </head>
