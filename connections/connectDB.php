48002eb5b91b19d6326b39599f23d508c763d8fb
Lenilson Sillva <lenilson_sillva@yahoo.com>
2021-09-04 20:33:16 -0300

    change all

--- connections/connectDB.php
+++ connections/connectDB.php
@@ -1,9 +1,9 @@
 <?php
 
 $hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "cadastro";
+$username = "306598";
+$password = "sinho123";
+$database = "306598";
 $connectDB = mysqli_connect($hostname,$username,$password,$database);
 
 if ($connectDB -> connect_error) {
